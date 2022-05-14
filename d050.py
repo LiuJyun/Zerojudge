90609c79b3fc20a8c8fc4508e9841f59e342bc5b
AnnieChen <annie8528@gmail.com>
2022-05-14 23:05:51 +0800

    [annie] 0514

---
+++ d050.py
@@ -0,0 +1,5 @@
+n = int(input()) - 15
+if n < 0:
+    print(n + 24)
+else:
+    print(n)
---
+++ d053.py
@@ -0,0 +1,6 @@
+while True:
+    try:
+        m, n = map(int, input().split())
+        print(m*n - 1)
+    except:
+        break
