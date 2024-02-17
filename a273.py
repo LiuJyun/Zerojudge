af53b9055d94694cf25acdf0e709a2d4d76ae6ec
LiuJyun <34228524+LiuJyun@users.noreply.github.com>
2024-02-17 16:58:06 +0800

    0217

--- a273.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        a, b = map(int, input().split())
-        if a % b == 0 or a == 0:
-            print('Ok!')
-        else:
-            print('Impossib1e!')
-    except:
-        break
